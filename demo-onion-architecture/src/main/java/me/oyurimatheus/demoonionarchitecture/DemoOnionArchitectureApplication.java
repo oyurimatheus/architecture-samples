9e54e784a8745a44d218460f62cf030bf81dcf51
Yuri Matheus <yurimatheusb@hotmail.com>
2019-07-18 00:16:41 -0300

    first commit

---
+++ demo-onion-architecture/src/main/java/me/oyurimatheus/demoonionarchitecture/DemoOnionArchitectureApplication.java
@@ -0,0 +1,13 @@
+package me.oyurimatheus.demoonionarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOnionArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOnionArchitectureApplication.class, args);
+	}
+
+}
